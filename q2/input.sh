352226c71dbc15d0aaa8554994a68bbb1776abc9
yk <jerr8538@gmail.com>
2026-08-22 19:02:45 +0530

    actual test input

--- q2/input.sh
+++ q2/input.sh
@@ -1,20 +1,23 @@
-# dimensions TODO
-printf "3 3 3\n" >> in
+# dimensions
+m=$(($RANDOM%10+1))
+n=$(($RANDOM%10+1))
+p=$(($RANDOM%10+1))
+printf "$m $n $p\n" >> in
 
 # 1st matrix
-for _ in {1..3}; do
-    for __ in {1..3}; do
+for _ in $(seq 1 $m); do
+    for __ in $(seq 1 $n); do
         printf "$RANDOM " >> in
     done
 done
 printf "\n" >> in
 
 # 2nd matrix
-for _ in {1..3}; do
-    for __ in {1..3}; do
+for _ in $(seq 1 $n); do
+    for __ in $(seq 1 $p); do
         printf "$RANDOM " >> in
     done
 done
 printf "\n" >> in
 
-printf "2\n" >> in
+printf "$(($RANDOM%8+1))\n" >> in
